059d39ea484cf2e2800fe3326f9deb7115bd1bec
Nicolas Georgeos Mantzos <georgeos.nicolas83@gmail.com>
2020-05-27 09:40:53 -0300

    Lista de despesas - Pt1 (configuração da função recuperarTodosRegistros()).

--- js/app.js
+++ js/app.js
@@ -38,6 +38,28 @@ class Bd {
 
 		localStorage.setItem('id', id)
 	}
+
+	recuperarTodosRegistros(){
+		
+		//array de despesas
+		let despesas = Array()
+
+		let id = localStorage.getItem("id")
+		
+		//recuperar todas as despesas cadastradas em localStorage
+		for(let i = 1; i <= id; i++){
+			//recuperar a despesa
+			let despesa = JSON.parse(localStorage.getItem(i))
+			
+			//existe a possibilidade de haver índices que foram pulados/removidos
+			//nestes casos nós vamos pular esses índices
+			if(despesa === null)
+				continue
+			despesas.push(despesa)
+		}
+		return despesas
+
+	}
 }
 
 let bd = new Bd()
@@ -66,7 +88,7 @@ function cadastrarDespesa() {
 		//dialog de sucesso
 		$('#modalRegistraDespesa').modal('show') 
 	} else {
-		document.getElementById('modal_titulo').innerHTML = 'Erro na inclusão do registro'
+		document.getElementById("modal_titulo").innerHTML = 'Erro na inclusão do registro'
 		document.getElementById('modal_titulo_div').className = 'modal-header text-danger'
 		document.getElementById('modal_conteudo').innerHTML = 'Erro na gravação, verifique se todos os campos foram preenchidos corretamente!'
 		document.getElementById('modal_btn').innerHTML = 'Voltar e corrigir'
@@ -76,4 +98,10 @@ function cadastrarDespesa() {
 		$('#modalRegistraDespesa').modal('show') 
 	}	
 	
+}
+
+function carregaListaDespesas(){
+	let despesas = Array()
+	despesas = bd.recuperarTodosRegistros()
+	console.log(despesas)
 }
