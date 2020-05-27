00126aeb15f2866db4e5d11a9da783bd59c35189
Nicolas Georgeos Mantzos <georgeos.nicolas83@gmail.com>
2020-05-27 09:16:16 -0300

    Configuração da exibição de sucesso na validação ao usuário (refactory).

--- js/app.js
+++ js/app.js
@@ -55,10 +55,25 @@ function cadastrarDespesa() {
 	let despesa = new Despesa(ano, mes, dia, tipo, descricao, valor)
 
 	if(despesa.validarDados()){
-		bd.gravar(despesa)
-		$("#sucessoGravacao").modal("show")
+		//bd.gravar(despesa)
+
+		document.getElementById('modal_titulo').innerHTML = 'Registro inserido com sucesso'
+		document.getElementById('modal_titulo_div').className = 'modal-header text-success'
+		document.getElementById('modal_conteudo').innerHTML = 'Despesa foi cadastrada com sucesso!'
+		document.getElementById('modal_btn').innerHTML = 'Voltar'
+		document.getElementById('modal_btn').className = 'btn btn-success'
+
+		//dialog de sucesso
+		$('#modalRegistraDespesa').modal('show') 
 	} else {
-		$("#erroGravacao").modal("show")
+		document.getElementById('modal_titulo').innerHTML = 'Erro na inclusão do registro'
+		document.getElementById('modal_titulo_div').className = 'modal-header text-danger'
+		document.getElementById('modal_conteudo').innerHTML = 'Erro na gravação, verifique se todos os campos foram preenchidos corretamente!'
+		document.getElementById('modal_btn').innerHTML = 'Voltar e corrigir'
+		document.getElementById('modal_btn').className = 'btn btn-danger'
+
+		//dialog de erro
+		$('#modalRegistraDespesa').modal('show') 
 	}	
 	
 }
